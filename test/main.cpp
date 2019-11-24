0c439912f6bc16a30310c7d8efdf65d79c41f90d
drew <drew@newshedge.com>
2019-11-24 07:09:57 -0600

    Refactoring

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,11 @@
 /* 
  * 
- * Author: Drew Dormann
+ * veque test suite.
+ * 
+ * Additionally, valgrind claims there is no bad behavior throughout this usage.
  *
- * Copyright (C) 2019
+ *  Copyright (C) 2019 Drew Dormann
+ * 
  */
 
 #define CATCH_CONFIG_MAIN
